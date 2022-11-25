f1d4fe0add236bda80863faf30307294d40818db
gafuda <113848820+gafuda@users.noreply.github.com>
2022-11-25 12:57:33 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -26,28 +26,30 @@
 // }
 
 
-
-// Console.WriteLine("Введите координаты: ");
-
-// float x1 = float.Parse(Console.ReadLine() ?? "0");
-
-// Console.WriteLine("Введите координаты a: ");
-// float x2 = float.Parse(Console.ReadLine() ?? "0");
-// Console.WriteLine("Введите координаты b: ");
-// float y1 = float.Parse(Console.ReadLine() ?? "0");
-// Console.WriteLine("Введите координаты c: ");
-// float y2 = float.Parse(Console.ReadLine() ?? "0");
-// Console.WriteLine("Масштабировать фигуру? : ");
-// float k = float.Parse(Console.ReadLine() ?? "0");
-
-// float[] array = {x1, x2, y1, y2};
-// Console.WriteLine($"Фигура была увеличенна в {k} раз(а), новые координаты фигуры: ");
-// for (int i = 0; i < array.Length; i++)
+// double[] massiv_coordinat_s_mashtabom(string[] num, double k)
 // {
-//     array[i] *= k;
-//     Console.Write($"({array[i]}), ");
+//     double[] mas = new double[num.Length];
+//     for (int ind = 0; ind < num.Length; ind++)
+//     {
+//         mas[ind] = (double.Parse(num[ind])) * k;
+//     }
+//     return mas;
 // }
 
+// void Pechat_Massiva(double[] massiv)
+// {
+//     for (int i = 0; i < massiv.Length; i+=2)
+//     {
+//         Console.Write($"({massiv[i]}, {massiv[i+1]})  ");
+//     }
+// }
+
+// Console.Write("Введите координаты точек фигуры через запятую ',': ");
+// string[] num = (Console.ReadLine() ?? "0").Split(',');
+// Console.Write("Введите коэфициент масштабирования: ");
+// double k = double.Parse(Console.ReadLine() ?? "0");
+// double[] mash_koor = massiv_coordinat_s_mashtabom(num, k);
+// Pechat_Massiva(mash_koor);
 
 
 // void FillArray (int[] array, int min, int max)
